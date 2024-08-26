8b70814c4e5428a1646144c8ba99d97c89e218f2
Diogo Mattioli <diogomattioli@gmail.com>
2024-08-26 14:54:24 +0300

    chore: logger setup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use env_logger::Env;
+
 mod engine;
 
 fn main() {
-    println!("Hello, world!");
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    log::info!("Hello, world!");
 }
