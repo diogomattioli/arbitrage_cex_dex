fbe16dc399cc6af67f47dafc937dc6c7e7ee0cd1
Diogo Mattioli <diogomattioli@gmail.com>
2024-08-27 03:23:47 +0300

    tests: kraken

--- src/exchange/kraken.rs
+++ src/exchange/kraken.rs
@@ -24,7 +24,7 @@ impl ExchangeWebSocketConfig for Kraken {
                 .iter()
                 .map(|pair| format!(r#""{pair}""#))
                 .collect::<Vec<_>>()
-                .join(",")
+                .join(", ")
         )
     }
 
@@ -70,15 +70,51 @@ mod tests {
 
     use super::*;
     use env_logger::Env;
-    use rust_decimal_macros::dec;
 
+    #[ignore]
     #[tokio::test]
     async fn test_run() {
         env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
-        let (tx, rx) = tokio::sync::watch::channel(MarketPrice::default());
+        let (tx, _rx) = tokio::sync::watch::channel(MarketPrice::default());
 
         websocket_run::<Kraken>(tx, ["BTC/USDT"]).await;
-        assert_eq!(dec!(1), dec!(1));
+    }
+
+    #[test]
+    fn test_get_subscribe_payload() {
+        let payload = Kraken::get_subscribe_payload(["BTC/USDT", "ETH/USDT"]);
+        assert_eq!(
+            payload,
+            r#"{"method": "subscribe", "params": {"channel": "ticker", "snapshot": false, "event_trigger": "bbo", "symbol": ["BTC/USDT", "ETH/USDT"]}, "req_id": 1 }"#
+        );
+    }
+
+    #[test]
+    fn test_parse_incoming_payload() {
+        let payload =
+            r#"{
+                "channel": "ticker",
+                "type": "snapshot",
+                "data": [
+                    {
+                        "symbol": "ALGO/USD",
+                        "bid": 0.10025,
+                        "bid_qty": 740.0,
+                        "ask": 0.10036,
+                        "ask_qty": 1361.44813783,
+                        "last": 0.10035,
+                        "volume": 997038.98383185,
+                        "vwap": 0.10148,
+                        "low": 0.09979,
+                        "high": 0.10285,
+                        "change": -0.00017,
+                        "change_pct": -0.17
+                    }
+                ]
+            }"#;
+        let market_price = Kraken::parse_incoming_payload(payload.to_string()).unwrap();
+        assert_eq!(market_price.market, "ALGO/USD");
+        assert_eq!(market_price.price, dec!(0.100305));
     }
 }
